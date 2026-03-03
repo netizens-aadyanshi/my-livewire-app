15b03fb3bdfc080a49f6a4e3bcfbdb175aaea6f0
netizens-aadyanshi <aadyanshi.p@netizens.email>
2026-03-03 16:15:37 +0530

    all new main

---
+++ app/Models/Post.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'excerpt',
+        'content',
+        'featured_image',
+        'status',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($post) {
+            if (empty($post->slug)) {
+                $post->slug = \Str::slug($post->title);
+            }
+        });
+    }
+}
